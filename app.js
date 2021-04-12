44996700dd8694524be3d9109b95f90b89ac2bd4
AudryLopez <thebigaudry@gmail.com>
2021-04-12 09:36:51 -0400

    add link to empleados

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const DB = require('./database')
+const app = express();
+
+app.get('/empleados/:id', (req, res)=>{
+  res.status(404).send("<h3>Bad request</h3>")
+})
+app.get('/', (req, res) =>{
+  res.send('<a href="/empleados">empleados</a>')
+})
+app.get('/empleados', DB.empleados)
+app.get('/peliculas', DB.peliculas)
+app.get('/peliculas/:id', DB.peliculasId)
+
+app.put('/createpelicula/:nombre', DB.createpelicula)
+
+app.listen(3000, (req, res) => {
+  console.log("Running")
+})
