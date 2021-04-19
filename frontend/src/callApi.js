4da0e6b02a030be037513a9026dec8959735788c
AudryLopez <thebigaudry@gmail.com>
2021-04-19 17:11:26 -0400

    se puso un foreach en la llamada de la api para desplegar el json

--- frontend/src/callApi.js
+++ frontend/src/callApi.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const callApi = ()=>{
     axios.get('http://localhost:8000/peliculas')
         .then(function (response) {
-            console.log(response);
+            response.forEach(element => console.log(element));
   })}
 
 export default callApi;
